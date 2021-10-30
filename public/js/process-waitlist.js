d2c4607138847de097222120cb6565df5fa04a82
Arthur Bernier Jr <ma2bernier@gmail.com>
2021-10-29 23:41:16 -0400

    Minor Changes

--- public/js/process-waitlist.js
+++ public/js/process-waitlist.js
@@ -2,17 +2,23 @@ const mainForm = document.querySelector('#main-form')
 const emailField = document.querySelector('#email')
 mainForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  fetch('/api/addUser', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email: emailField.value
+  const regex = /^[\w\-\_\.\@]+$/g
+  if(regex.test(email.value)){
+    fetch('/api/addUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: emailField.value
+      })
+    }).then((response)=>{
+      return response.json()
+    }).then((data) => {
+       mainForm.innerHTML = `<h2 style="text-align: center; letter-spacing: .25rem; color: white;"> Thankyou For Subscribing </h2>`
     })
-  }).then((response)=>{
-    return response.json()
-  }).then((data) => {
-     mainForm.innerHTML = `<h2 style="text-align: center; letter-spacing: .25rem; color: white;"> Thankyou For Subscribing </h2>`
-  })
+  } else {
+    alert('Please enter a valid email')
+  }
+
 })
