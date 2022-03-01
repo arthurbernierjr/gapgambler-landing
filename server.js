32e583e236f7de63d8e0f28bc7c085b5620a6794
Arthur Bernier Jr <ma2bernier@gmail.com>
2022-02-28 21:48:34 -0500

    Minor Changes

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const PORT = process.env.PORT || 8080
 app.use(express.json())
 app.use(express.static('public'))
+app.use(cors())
 
 const schema = new mongoose.Schema({
 	email: { type: String, required: true }
